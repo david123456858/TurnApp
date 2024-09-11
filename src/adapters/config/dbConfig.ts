68ae280f9bae94b86a11a9b7a3fd75864df1dd3f
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-11 09:22:16 -0500

    mas config

--- src/adapters/config/dbConfig.ts
+++ src/adapters/config/dbConfig.ts
@@ -1,11 +1,6 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
 
-export const URI = "mongodb+srv://juandavidperaltafuentes:AOVwBcd4U2umAdKt@turnapp.rrizk.mongodb.net/?retryWrites=true&w=majority&appName=TurnApp";
 
-export const client = new MongoClient(URI,{
-    serverApi:{
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-    }
-})
+export const URI =
+  "mongodb+srv://juandavidperaltafuentes:AOVwBcd4U2umAdKt@turnapp.rrizk.mongodb.net/?retryWrites=true&w=majority&appName=TurnApp";
+
+
---
+++ src/frameWorks/db/db.ts
@@ -0,0 +1,45 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { URI } from "../../adapters/config/dbConfig";
+
+export class dbBase {
+  private static _intanceDb: dbBase;
+  appDb!: MongoClient;
+
+  constructor() {
+    this.appDb = new MongoClient(URI, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+  }
+
+  public async connectDb(): Promise<void | MongoClient> {
+    try {
+      await this.appDb.connect();
+      await this.appDb.db("TurnApp").command({ ping: 1 });
+      return this.appDb
+    } catch (error) {
+        console.log('error db: ', error)
+    }
+    finally{
+        await this.appDb.close()
+    }
+  }
+
+  public static get instance(): dbBase {
+    if (!this._intanceDb) {
+      return (this._intanceDb = new dbBase());
+    }
+    return this._intanceDb;
+  }
+}
+
+export const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
