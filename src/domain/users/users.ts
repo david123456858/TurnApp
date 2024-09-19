608a85f6917187eec5300e0ef96fb6bc6ae043da
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-19 14:56:34 -0500

    repository user

--- src/domain/users/users.ts
+++ src/domain/users/users.ts
@@ -1,15 +1,7 @@
-
-import { model, Schema } from 'mongoose'
-
-const userSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  nit: { type: String, required: true, unique: true },
-  nameCompany: { type: String, required: true },
-  numberPhone: { type: String, required: true }
-  // valor de la imagen todavia por decidirse
-})
-
-const userModel = model('users', userSchema)
-
-export default userModel
+export class User {
+  email!: string
+  password!: string
+  nit!: string
+  nameCompany!: string
+  numberPhone!: string
+}
--- src/repository/user/repository.user.ts
+++ src/repository/user/repository.user.ts
@@ -1,3 +1,23 @@
-// import { IcrudRepository } from './../../adapters/interface/repository/crudRepository'
-export class repositoryUser {
+import userModel from '../../adapters/models/repository/users/user'
+import { User } from '../../domain/users/users'
+import { IcrudRepository } from './../../adapters/interface/repository/crudRepository'
+
+export class repositoryUser implements IcrudRepository<User> {
+  async save (data: User): Promise<void | Error> {
+    await userModel.create(data)
+    console.log('se guardo correctamente el usuario')
+  }
+
+  async update (data: User): Promise<any> {
+
+  }
+
+  async delete (id: string): Promise<any> {}
+
+  async findById (id: string): Promise<any> {
+    const dataUser = userModel.findOne({ nit: id })
+    return await dataUser
+  }
+
+  async findAll (): Promise<any> {}
 }
