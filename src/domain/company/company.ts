cc7cd4aa8aadb4946190e88d9e2ad3409bfc3d03
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-18 19:36:25 -0500

    domain add - company

--- src/domain/company/company.ts
+++
@@ -1,8 +0,0 @@
-import { model, Schema } from 'mongoose'
-
-const companySchema = new Schema({
-  nameCompany: { type: String, required: true },
-  nit: { type: String, unique: true }
-})
-
-export const company = model('company', companySchema)
