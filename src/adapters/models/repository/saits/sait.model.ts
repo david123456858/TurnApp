ffc44e5e656488b09c88bd3decea92ac423b8b42
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-26 21:21:25 -0500

    models check

---
+++ src/adapters/models/repository/saits/sait.model.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from 'mongoose'
+
+const saitSchema = new Schema({
+  nameSait: { type: String, required: true },
+  city: { type: String },
+  address: { type: String },
+  numberEmployesForDay: { type: Number, required: true }
+
+})
+
+const saitModel = model('sait', saitSchema)
+
+export default saitModel
