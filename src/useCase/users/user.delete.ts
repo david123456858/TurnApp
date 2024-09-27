a77ba49f0f8b366c6d812ec315df8f002d5d3433
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-26 21:18:50 -0500

    crud users

---
+++ src/useCase/users/user.delete.ts
@@ -0,0 +1,26 @@
+import { FailureProccess, SuccessProcess } from './../../adapters/utils/result/resultApi'
+import { repositoryUser } from '../../repository/user/repository.user'
+import { IFailureProcess, ISuccessProcess } from '../../adapters/interface/results/restults'
+
+export class CaseUseDeleteUser {
+  private readonly repostitory: repositoryUser
+  constructor (repositorysUser: repositoryUser) {
+    this.repostitory = repositorysUser
+  }
+
+  async deleteUser (idUser: string): Promise<IFailureProcess<any> | ISuccessProcess<any>> {
+    try {
+      if (idUser === null) return FailureProccess('Inprocessible entity', 422)
+
+      if (idUser === undefined) return FailureProccess('Inprocessible entity', 422)
+
+      const userDeleted = this.repostitory.delete(idUser)
+
+      console.log(userDeleted)
+
+      return SuccessProcess('User deleted', 200)
+    } catch (error) {
+      return FailureProccess('Error internar server', 500)
+    }
+  }
+}
