cb021a44d84e15e6de1eaa56fac55d7d46f863b4
david123456858 <juandavidperaltafuentes@gmail.com>
2024-12-07 16:20:19 -0500

    migracion a bd relacional 2

--- src/repository/rule/repository.rule.ts
+++ src/repository/rule/repository.rule.ts
@@ -1,35 +1,27 @@
 import { IcrudRepository } from '../../adapters/interface/repository/crudRepository'
-import ruleModel from '../../adapters/models/repository/rules/rules.model'
 import { rules } from '../../domain/rules/rules'
 
 export class repositoryRules implements IcrudRepository<rules> {
   async save (data: rules): Promise<any> {
-    await ruleModel.create(data)
-    return 'Se guardo correctamente'
   }
 
   async update (data: rules): Promise<any> {
-    const ruleUpdated = await ruleModel.findOneAndUpdate({ nameRule: data.nameRule }, { $set: data }, { new: true })
-    return ruleUpdated
+
   }
 
   async delete (id: string): Promise<any> {
-    const ruleDeleted = ruleModel.deleteOne({ idRule: id })
-    return await ruleDeleted
+
   }
 
   async findById (id: string): Promise<any> {
-    const ruleFind = ruleModel.findOne({ idRule: id })
-    return await ruleFind
+
   }
 
   async findAll (): Promise<any> {
-    const rulesFinds = ruleModel.find()
-    return await rulesFinds
+
   }
 
   async findByNameRule (data: string): Promise<any> {
-    const ruleFind = await ruleModel.findOne({ nameRule: data })
-    return ruleFind
+
   }
 }
--- src/repository/user/repository.user.ts
+++ src/repository/user/repository.user.ts
@@ -1,37 +1,37 @@
-import userModel from '../../adapters/models/repository/users/user'
+// import userModel from '../../adapters/models/repository/users/user'
 import { User } from '../../domain/users/users'
 import { IcrudRepository } from './../../adapters/interface/repository/crudRepository'
 
 export class repositoryUser implements IcrudRepository<User> {
+  // esl int-disable-next-line @typescript-eslint/no-invalid-void-type
   // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
   async save (data: User): Promise<void | Error> {
-    await userModel.create(data)
-    console.log('se guardo correctamente el usuario')
+
   }
 
   async update (data: User): Promise<any> {
-    const result = userModel.findOneAndUpdate({ email: data.email }, { $set: data }, { new: true })
-    return await result
+    const result = ''
+    return result
   }
 
   async delete (id: string): Promise<any> {
-    const resultRemoveUser = userModel.deleteOne({ nit: id })
-    return await resultRemoveUser
+    const resultRemoveUser = 'userModel.deleteOne({ nit: id })'
+    return resultRemoveUser
   }
 
   async findById (id: string): Promise<any> {
-    const dataUser = userModel.findOne({ nit: id })
-    return await dataUser
+    const dataUser = 'userModel.findOne({ nit: id })'
+    return dataUser
   }
 
   async findAll (): Promise<any> {
-    const dataUsers = await userModel.find()
+    const dataUsers = ' userModel.find()'
     console.log(dataUsers)
     return dataUsers
   }
 
   async findByEmail (emailSend: string): Promise<any> {
-    const resultSearchEmail = userModel.findOne({ email: emailSend })
-    return await resultSearchEmail
+    const resultSearchEmail = 'userModel.findOne({ email: emailSend })'
+    return resultSearchEmail
   }
 }
