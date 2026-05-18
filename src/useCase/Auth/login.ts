a9468f29c9602f5fef29d78cc84caa0a7a011580
david123456858 <juandavidperaltafuentes@gmail.com>
2026-05-17 22:12:13 -0500

    refactor: mejorar la lógica de inicio de sesión y ajustar tipos de retorno

--- src/useCase/Auth/login.ts
+++ src/useCase/Auth/login.ts
@@ -5,25 +5,25 @@ import { IFailureProcess, ISuccessProcess } from '@interface/results/restults'
 import { FailureProccess, SuccessProcess } from '@utils/result/resultApi'
 import { logginDto } from '@Dtos/auth/logginDtos'
 import { repositoryUser } from '@repository/user/repository.user'
+import { Users } from '@Entity/Users/users'
 
 export class caseUserLoggin {
   private readonly repostitory: repositoryUser
 
-  constructor (respository: repositoryUser) {
+  constructor(respository: repositoryUser) {
     this.repostitory = respository
   }
 
-  async loggin (userLoggin: logginDto): Promise<IFailureProcess<any> | ISuccessProcess<any>> {
+  async loggin(userLoggin: logginDto): Promise<IFailureProcess<any> | ISuccessProcess<Omit<Users, "password">>> {
     try {
       const userLogged = await this.repostitory.findByEmail(userLoggin.email)
       if (!userLogged) return FailureProccess('Not found', 404)
-        
-      const comparePassword = bcryptjs.compareSync(userLoggin.password, userLogged.password)
-      
-      if (!comparePassword) return FailureProccess('User Not found', 404)
-      const { password, ...rest } = userLogged
 
-      return SuccessProcess(rest, 200)
+      const comparePassword = bcryptjs.compareSync(userLoggin.password, userLogged.password)
+
+      if (!comparePassword) return FailureProccess('User Not found', 404)
+
+      return SuccessProcess(userLogged, 200)
     } catch (error) {
       return FailureProccess('error internal server', 500)
     }
